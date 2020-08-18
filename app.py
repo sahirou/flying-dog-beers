2dcaf29e301f81d430e56a6343ae8d570b1e7c2f
Mahaman Sani SAHIROU ADAMOU <55301737+sahirou@users.noreply.github.com>
2020-08-18 13:03:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,63 +1,286 @@
+"""
+Dash port of Shiny iris k-means example:
+
+https://shiny.rstudio.com/gallery/kmeans-example.html
+"""
 import dash
+import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
+import pandas as pd
 import plotly.graph_objs as go
+from dash.dependencies import Input, Output
+# from sklearn import datasets
+# from sklearn.cluster import KMeans
+import datetime as dt
 
-########### Define your variables
-beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
-ibu_values=[35, 60, 85, 75]
-abv_values=[5.4, 7.1, 9.2, 4.3]
-color1='lightblue'
-color2='darkgreen'
-mytitle='Beer Comparison'
-tabtitle='beer!'
-myheading='Flying Dog Beers'
-label1='IBU'
-label2='ABV'
-githublink='https://github.com/austinlasseter/flying-dog-beers'
-sourceurl='https://www.flyingdog.com/beers/'
+COLORS = {
+    'foucha' : '#ff00ff'
+}
 
-########### Set up the chart
-bitterness = go.Bar(
-    x=beers,
-    y=ibu_values,
-    name=label1,
-    marker={'color':color1}
+
+ACTIVITIES = [
+    'eRecharge',
+    'SIM',
+    'SimSwap',
+    'Orange Money'
+]
+
+STATUS = [
+    'Actif',
+    'Future inactif',
+    'Inactif',
+    'Inactif récent',
+    'Inactif âgé'
+]
+
+ERECHARGE_CATEGORIES = [
+    'Performant',
+    'Presque performant',
+    'Non performant'
+]
+
+DACRS = [
+    'Agadez',
+    'Diffa',
+    'Dosso',
+    'Maradi',
+    'Niamey',
+    'Tahoua',
+    'Tillaberi',
+    'Zinder'
+]
+
+ZONES = [
+    'Sud',
+    'Nord',
+    'Est',
+    'Ouest'
+]
+
+SECTEURS = [
+    'Kollo',
+    'Gouré',
+    'Tanout',
+    'Oullam'
+]
+
+# Create controls
+activity_options = [
+    {"label": activity, "value": activity} for activity in ACTIVITIES
+]
+
+status_options = [
+    {"label": status, "value": status} for status in STATUS
+]
+
+erecharge_category_options = [
+    {"label": category, "value": category} for category in ERECHARGE_CATEGORIES
+]
+
+dacr_options = [
+    {"label": dacr, "value": dacr} for dacr in DACRS
+]
+
+zone_options = [
+    {"label": zone, "value": zone} for zone in ZONES
+]
+
+secteur_options = [
+    {"label": secteur, "value": secteur} for secteur in SECTEURS
+]
+
+
+
+# iris_raw = datasets.load_iris()
+# iris = pd.DataFrame(iris_raw["data"], columns=iris_raw["feature_names"])
+
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+controls = dbc.Card(
+    [
+        dbc.FormGroup(
+            [
+                dbc.Label("Date",style={'margin-right':'0.5rem','marging-top' : '3rem'}),
+                html.Br(),
+                dcc.DatePickerSingle(
+                    id='date-picker-single',
+                    min_date_allowed=dt.date(2020,1,1),
+                    max_date_allowed=dt.date(2020,8,31),
+                    initial_visible_month=dt.date(2020,2,1),
+                    date=dt.date(2020,8, 25),
+                    className="dash-bootstrap"
+                ),
+            ]
+        ),
+        dbc.FormGroup(
+            [
+                html.Hr(className="dash-bootstrap",style={'border-top': '1px dashed rgb(135,153,153)'}),
+                dbc.Label("Activités"),
+                dcc.Dropdown(
+                    id="activities",
+                    options=activity_options,
+                    multi=True,
+                    value=[ACTIVITIES[0]],
+                ),
+            ]
+        ),
+        # status du PDV sur les activités selectionnées
+        dbc.FormGroup(
+            [
+                dbc.Label("Status"),
+                dbc.Checklist(
+                    options=status_options,
+                    value=['Actif','Future inactif'],
+                    id="input-status-erecharge",
+                    switch=True,
+                    inline=True
+                ),
+            ]
+        ),
+        # Categorie eRecharge
+        dbc.FormGroup(
+            [
+                html.Hr(className="dash-bootstrap",style={'border-top': '1px dashed rgb(135,153,153)'}),
+                dbc.Label("Catégorie eRecharge"),
+                dbc.Checklist(
+                    options=erecharge_category_options,
+                    value=ERECHARGE_CATEGORIES,
+                    id="input-category-erecharge",
+                    switch=True,
+                    inline=True
+                ),
+            ]
+        ),
+        # Filtre DACR
+        dbc.FormGroup(
+            [
+                html.Hr(className="dash-bootstrap",style={'border-top': '1px dashed rgb(135,153,153)'}),
+                # html.P('Filtres Géo',style={'margin-top':'0','text-align': 'right','font-style': 'italic','color':'red'}),
+                dbc.Label("Régions"),
+                dcc.Dropdown(
+                    id="dacr",
+                    options=dacr_options,
+                    multi=True,
+                    value=DACRS,
+                ),
+            ]
+        ),
+        # Filtre ZONE
+        dbc.FormGroup(
+            [
+                dbc.Label("Zones"),
+                dcc.Dropdown(
+                    id="zone",
+                    options=zone_options,
+                    multi=True,
+                    value=ZONES,
+                ),
+            ]
+        ),
+        # Filtre SECTEUR
+        dbc.FormGroup(
+            [
+                dbc.Label("Secteurs"),
+                dcc.Dropdown(
+                    id="secteur",
+                    options=secteur_options,
+                    multi=True,
+                    value=[SECTEURS[0]],
+                ),
+            ]
+        ),
+        # Submit
+        dbc.FormGroup(
+            [
+                html.Hr(className="dash-bootstrap",style={'border-top': '1px dashed rgb(135,153,153)'}),
+                dbc.Button("Valider", id="submit", className="mr-2",style={'display': 'inline-block'}),
+                # html.Span(id="example-output", style={"vertical-align": "middle"}),
+            ]
+        ),
+    ],
+    body=True,
 )
-alcohol = go.Bar(
-    x=beers,
-    y=abv_values,
-    name=label2,
-    marker={'color':color2}
-)
 
-beer_data = [bitterness, alcohol]
-beer_layout = go.Layout(
-    barmode='group',
-    title = mytitle
-)
-
-beer_fig = go.Figure(data=beer_data, layout=beer_layout)
-
-
-########### Initiate the app
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-server = app.server
-app.title=tabtitle
-
-########### Set up the layout
-app.layout = html.Div(children=[
-    html.H1(myheading),
-    dcc.Graph(
-        id='flyingdog',
-        figure=beer_fig
+# Viz
+tab1_content = dbc.Card(
+    dbc.CardBody(
+        [
+            html.P("Quelques stats", className="card-text"),
+            # bc.Button("Click here", color="success"),
+        ]
     ),
-    html.A('Code on Github', href=githublink),
-    html.Br(),
-    html.A('Data Source', href=sourceurl),
+    className="mt-3",
+)
+
+tab2_content = dbc.Card(
+    dbc.CardBody(
+        [
+            html.P("Carte", className="card-text"),
+            # dbc.Button("Don't click here", color="danger"),
+        ]
+    ),
+    className="mt-3",
+)
+
+tab3_content = dbc.Card(
+    dbc.CardBody(
+        [
+            html.P("Données tabulaires", className="card-text"),
+            # dbc.Button("Don't click here", color="danger"),
+        ]
+    ),
+    className="mt-3",
+)
+
+
+tabs = dbc.Tabs(
+    [
+        dbc.Tab(tab1_content, label="Overview"),
+        dbc.Tab(tab2_content, label="Map"),
+        dbc.Tab(tab3_content, label="Données tabulaires"),
     ]
 )
 
-if __name__ == '__main__':
+# jumbo
+jumbotron = dbc.Jumbotron(
+    [
+        dbc.Container(
+            [
+                html.H1("Capillarité Orange Niger",className="display-5"),
+                html.Hr(className="dash-bootstrap",style={'margin-bottom':'2rem','border-top': '1px solid rgb(255,89,0)'}),
+                dbc.Row(
+                    [
+                        dbc.Col(controls, md=4),
+                        dbc.Col(tabs, md=8),
+                    ],
+                    align="top",
+                ),
+            ],
+            fluid=True
+        )
+    ],
+    fluid=True
+)
+
+# main layout
+app.layout = dbc.Container(
+            [
+                html.H1("Capillarité Orange Niger",className="display-5",style={'margin-top':'2rem'}),
+                html.Hr(className="dash-bootstrap",style={'margin-bottom':'2rem','border-top': '1px solid rgb(200,200,200)'}),
+                dbc.Row(
+                    [
+                        dbc.Col(controls, md=4),
+                        dbc.Col(tabs, md=8),
+                    ],
+                    align="top",
+                ),
+            ],
+            fluid=True
+        )
+
+server = app.server
+app.title='Capillarité'
+
+if __name__ == "__main__":
     app.run_server()
