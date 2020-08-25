294d9fd51fab0a78dbab24c179b98f7e9c006ad4
Mahaman Sani SAHIROU ADAMOU <55301737+sahirou@users.noreply.github.com>
2020-08-26 00:38:43 +0100

    Update controls.py

--- controls.py
+++ controls.py
@@ -256,3 +256,17 @@ cached_columns = [
 ]
 
 
+MONTH_NAMES = {
+    'January': 'Janvier',
+    'February': 'Février',
+    'March' : 'Mars',
+    'April' : 'Avril',
+    'May' : 'Mai',
+    'June' : 'Juin',
+    'July' : 'Juillet',
+    'August' : 'Août',
+    'September' : 'Septembre',
+    'October' : 'Octobre',
+    'November' : 'Novembre',
+    'December': 'Décembre'
+}
